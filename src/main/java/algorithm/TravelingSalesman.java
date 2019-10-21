135f88da47adac8d7e589000c27baffdd4445fe0
chepiv <chepurin.ivan@gmail.com>
2019-10-21 08:10:02 +0200

    add second child for crossover

--- src/main/java/algorithm/TravelingSalesman.java
+++ src/main/java/algorithm/TravelingSalesman.java
@@ -70,16 +70,23 @@ public class TravelingSalesman {
         List<Integer> momRoute = mom.getRoute();
 
         List<Integer> mainPart = dadRoute.subList(start, end);
+        List<Integer> mainPart2 = momRoute.subList(start, end);
 
         Integer [] childRouteArray = new Integer[genomeSize];
         Arrays.fill(childRouteArray,-1);
         List<Integer> childRoute = Arrays.asList(childRouteArray);
 
+        Integer [] childRouteArray2 = new Integer[genomeSize];
+        Arrays.fill(childRouteArray2,-1);
+        List<Integer> childRoute2 = Arrays.asList(childRouteArray2);
+
         int j = 0;
         for (int i = start; i <end ; i++) {
             childRoute.set(i,mainPart.get(j));
+            childRoute2.set(i,mainPart2.get(j));
             j++;
         }
+
 
         //TODO: change to check if present from HASHSET
         for (int i = momRoute.size() - 1; i >= 0; i--) {
@@ -93,15 +100,29 @@ public class TravelingSalesman {
              }
         }
 
+        for (int i = dadRoute.size() - 1; i >= 0; i--) {
+             if (childRoute2.get(i) == -1){
+                 for (int k = dadRoute.size() - 1; k >= 0; k--) {
+                     if (!childRoute2.contains(dadRoute.get(k))){
+                         childRoute2.set(i, dadRoute.get(k));
+                         break;
+                     }
+                 }
+             }
+        }
+
         System.out.println("DAD");
         System.out.println(dadRoute);
         System.out.println("MOM");
         System.out.println(momRoute);
         System.out.println("CHILD");
         System.out.println(childRoute);
+        System.out.println("CHILD");
+        System.out.println(childRoute2);
 
         Genome child = new Genome(childRoute, startingCity, numberOfCities);
-        return Lists.newArrayList(child);
+        Genome child2 = new Genome(childRoute2, startingCity, numberOfCities);
+        return Lists.newArrayList(child,child2);
     }
 
 }
